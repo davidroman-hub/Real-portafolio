6b2a86ed6a1248027e88fff67ee4683ec99a25ad
davidroman-hub <jobroman83@gmail.com>
2020-01-23 20:22:39 +0100

    status

--- src/pages/ContactMe/ContactMe.js
+++ src/pages/ContactMe/ContactMe.js
@@ -5,124 +5,203 @@ import miami from'./miami2.jpg'
 import './ContactMe.scss'
 
 
+// function ContactMe() {
+//     const [name,setName] = useState('');
+//     const [status,setStatus] = useState('');
+//     const [email,setEmail] = useState('');
+//     const [message,setMessage] = useState('');
+//     const [file, setFile] = useState({});
+
+//     const onDrop = useCallback(acceptedFiles => {
+//         console.log(acceptedFiles)
+//         setFile(acceptedFiles[0])
+//     },[])
+
+//     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+//     const encode = (data) => { 
+//         const formData = new FormData();
+//         Object.keys(data).forEach((k)=> {
+//             formData.append(k,data[k])
+//         });
+//         return formData
+//     }
+
+//     const handleSubmit = e => {
+//         const data = { "form-name": "contact", name, email, message, file}
+        
+//         fetch('/',{
+//             method:"POST",
+//             body: encode(data)
+//         })
+
+//         .then(()=> setStatus("Gracias por tu mensaje"))
+//         .catch( error => setStatus("no se ha enviado tu mensaje"));
+//         e.preventDefault();
+   
+//     };
+//         const handleChange = e => { 
+//             const {name, value} = e.target
+//             if (name === "name") {
+//                 return setName(value)
+//             }
+//             if (name === "email"){
+//                 return setEmail(value)
+//             }
+//             if (name === "message"){
+//                 return setMessage(value)
+//             }
+
+//         }
 function ContactMe() {
     const [name,setName] = useState('');
     const [status,setStatus] = useState('');
     const [email,setEmail] = useState('');
     const [message,setMessage] = useState('');
     const [file, setFile] = useState({});
-
+  
     const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles)
-        setFile(acceptedFiles[0])
-    },[])
-
+      console.log(acceptedFiles)
+      setFile(acceptedFiles[0])
+    }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-    const encode = (data) => { 
-        const formData = new FormData();
-        Object.keys(data).forEach((k)=> {
-            formData.append(k,data[k])
-        });
-        return formData
+  
+    const encode = (data) => {
+      const formData = new FormData();
+      Object.keys(data).forEach((k)=>{
+        formData.append(k,data[k])
+      });
+      return formData
+    }
+  
+    const handleSubmit = e => {
+      const data = { "form-name": "contact", name, email, message, file }
+      
+      fetch("/", {
+        method: "POST",
+        // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
+        body: encode(data)
+      })
+        .then(() => setStatus("Gracias por tu mensaje!!"))
+        .catch(error => setStatus("No se ha enviado tu mensaje!"));
+  
+      e.preventDefault();
+    };
+  
+    const handleChange = e => {
+      const {name, value} = e.target
+      if (name === 'name' ){
+        return setName(value)
+      }
+      if (name === 'email' ){
+        return setEmail(value)
+      }
+      if (name === 'message' ){
+        return setMessage(value)
+      }
     }
 
-    const handleSubmit = e => {
-        const data = { "form-name": "contact", name, email, message, file}
-        
-        fetch('/',{
-            method:"POST",
-            body: encode(data)
-        })
-
-        .then(()=> setStatus("Gracias por tu mensaje"))
-        .catch( error => setStatus("no se ha enviado tu mensaje"))
-        e.preventDefault();
-   
-    };
-        const handleChange = e => { 
-            const {name, value} = e.target
-            if (name === "name") {
-                return setName(value)
-            }
-            if (name === "email"){
-                return setEmail(value)
-            }
-            if (name === "message"){
-                return setMessage(value)
-            }
-
-        }
-
         return(
-            <Fragment>
-            <div className="header-up">
-                <img alt="header" src={miami}/>
-                <div className="central-text">Contact Me</div>
-            </div>
+//             <Fragment>
+//             <div className="header-up">
+//                 <img alt="header" src={miami}/>
+//                 <div className="central-text">Contact Me</div>
+//             </div>
            
-           <div className="info">
-                Get in touch with me!
-           </div>
+//            <div className="info">
+//                 Get in touch with me!
+//            </div>
 
-           <div className="font-figures">
-                <i class="fas fa-map-pin">Adress</i>
-                <i classname='mailes' class="far fa-envelope-open">Email</i>
-                <i class="fab fa-whatsapp">Phone</i>
+//            <div className="font-figures">
+//                 <i class="fas fa-map-pin">Adress</i>
+//                 <i classname='mailes' class="far fa-envelope-open">Email</i>
+//                 <i class="fab fa-whatsapp">Phone</i>
 
-            </div> 
+//             </div> 
             
 
 
-            <div className="ContactMe">
+//             <div className="ContactMe">
                 
-            {/* <div className="Contact-me-logo">
-                <h3> Contact Me</h3>
-                    <NavLink exact to ="/home">
-                        <img alt="logo"  src={Logo}/>
-                    </NavLink>
-                </div> */}
+//             {/* <div className="Contact-me-logo">
+//                 <h3> Contact Me</h3>
+//                     <NavLink exact to ="/home">
+//                         <img alt="logo"  src={Logo}/>
+//                     </NavLink>
+//                 </div> */}
                 
-                <form className="form-content" onSubmit={handleSubmit} action="/thank-you/">
-                    <p>
-                        <label>
-                           <input placeholder="Your name:" type="text" name="name" value={name} onChange={handleChange}/>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                           <input  placeholder="Your Email:" type="email" name="email" value={email} onChange={handleChange}/>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                          <textarea placeholder="Message:" name="message" value={message} onChange={handleChange}/>
-                        </label> 
-                    </p>
-                        <div {...getRootProps()}>
-                        <input {...getInputProps()}/>
-                        {
-                            isDragActive ? 
-                            <p> Click hereif you want add a File..</p> :
-                            <p className="files" > Click here for add a Photo</p>
+//                 <form className="form-content" onSubmit={handleSubmit} action="/thank-you/">
+//                     <p>
+//                         <label>
+//                            <input placeholder="Your name:" type="text" name="name" value={name} onChange={handleChange}/>
+//                         </label>
+//                     </p>
+//                     <p>
+//                         <label>
+//                            <input  placeholder="Your Email:" type="email" name="email" value={email} onChange={handleChange}/>
+//                         </label>
+//                     </p>
+//                     <p>
+//                         <label>
+//                           <textarea placeholder="Message:" name="message" value={message} onChange={handleChange}/>
+//                         </label> 
+//                     </p>
+//                         <div {...getRootProps()}>
+//                         <input {...getInputProps()}/>
+//                         {
+//                             isDragActive ? 
+//                             <p> Click here if you want add a File..</p> :
+//                             <p className="files" > Click here for add a Photo</p>
 
-                        }
-                        </div>
-                        <p>
-                            <button type="submit">Send it!</button>
-                        </p>
-                </form>
-                <h3>{status}</h3>         
-            </div>
-            <div className="spacer">
+//                         }
+//                         </div>
+//                         <p>
+//                             <button type="submit">Send it!</button>
+//                         </p>
+//                 </form>
+//                 <h3>{status}</h3>         
+//             </div>
+//             <div className="spacer">
                 
-            </div>
-            <FooterV></FooterV>
+//             </div>
+//             <FooterV></FooterV>
                       
-</Fragment>
+// </Fragment>
+<div className="ContactMe">
+    <form onSubmit={handleSubmit} action="/thank-you/">
+          <p>
+            <label>
+              Tu Nombre: <input type="text" name="name" value={name} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Tu Email: <input type="email" name="email" value={email} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Mensaje: <textarea name="message" value={message} onChange={handleChange} />
+            </label>
+          </p>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive ?
+                <p>Haz click aqui para agregar algun archivo ...</p> :
+                <p className='files'>click aqui para adjuntar una foto</p>
+            }
+          </div>
+          <p>
+            <button type="submit">Envialo!</button>
+          </p>
+        </form>
+        <h3>{status}</h3>
+    </div>
         );
 
 }
 
 
 export default ContactMe
+
