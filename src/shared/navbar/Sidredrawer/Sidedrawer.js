b6266e4ed51b561806e50543838c9d194d1a2a1d
davidroman-hub <jobroman83@gmail.com>
2020-05-13 04:28:22 +0100

    fixing sidedrawer

--- src/shared/navbar/Sidredrawer/Sidedrawer.js
+++ src/shared/navbar/Sidredrawer/Sidedrawer.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Sidedrawer.scss'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const SideDrawer = (props) => {
     let drawerClasses = ['side-drawer'];
     if (props.show){
         drawerClasses = ['side-drawer', 'open']
     }
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+      };
+
 return(
 <nav className = { drawerClasses.join(' ')}>
-    <ul>
-      <li><NavLink exact to ="/Home" className="item2" activeClassName="is-selected">Home</NavLink></li>
-      {/* <li><NavLink exact to ="/AboutMe" className="item2" activeClassName="is-selected">About Me</NavLink></li> */}
-      <li><NavLink exact to ="/ContactMe" className="item2" activeClassName="is-selected">Contact Me</NavLink></li>
+    
+    <ul className="nav-items">
+                        <li className="nav-item"> <Link 
+                         activeClass="active"
+                         to="about"
+                         spy={true}
+                         smooth={true}
+                         offset={-70}
+                         duration={500}     
+                        >About</Link></li>
+                            <li className="nav-item"> <Link 
+                         activeClass="active"
+                         to="skills"
+                         spy={true}
+                         smooth={true}
+                         offset={-70}
+                         duration={500}     
+                        >Skills</Link></li>
+
+                        {/* <li><NavLink exact to ="/AboutMe" className="item3" activeClassName="is-selected">About me</NavLink></li> */}
+                        <li><NavLink exact to ="/ContactMe" className="item3" activeClassName="is-selected">Contact me</NavLink></li>
 
     </ul>
 </nav>
